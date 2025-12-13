5abf184c621eb786ae1e0c386c320ad16a03606a
rogergrubb <roger@grubb.net>
2025-12-13 00:41:38 -0800

    feat: Connect PinLegend to map navigation and pin refresh

--- apps/frontend/src/pages/MapPage.tsx
+++ apps/frontend/src/pages/MapPage.tsx
@@ -1421,7 +1421,7 @@ export default function MapPage() {
       `}</style>
 
       {/* Permanent Pin Legend - Left Side */}
-      <PinLegend />
+      <PinLegend onPinClick={(pin) => { if (mapRef.current) { mapRef.current.flyTo({ center: [pin.longitude, pin.latitude], zoom: 15, duration: 1500 }); } }} onPinDeleted={() => { fetchPins(); }} />
 
       {/* Incoming Visitors - Right Side */}
       <div className="absolute top-20 right-3 z-[999]">
